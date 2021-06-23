76ebe36e866aa9f967e806daab7674c74f71d632
arnaud290 <arnaud.manach@gmail.com>
2021-06-23 10:52:35 +0200

    update datetime

--- epiceventscrm/epiceventscrm/settings_exemple.py
+++ epiceventscrm/epiceventscrm/settings_exemple.py
@@ -130,7 +130,7 @@ USE_L10N = False
 
 USE_TZ = True
 
-DATETIME_FORMAT = 'd-m-Y H:m:s'
+DATETIME_FORMAT = 'd-m-Y H:i:s'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
